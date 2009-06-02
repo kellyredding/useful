a01f02451a3b8e46cd8dbd8b4cd8168f6d1645c5
Kelly <you already know it>
2009-06-01 21:09:48 -0500

    useful ruby extensions for fixnum

---
+++ lib/useful/ruby_extensions/fixnum.rb
@@ -0,0 +1,34 @@
+module Useful
+  module RubyExtensions
+    module Fixnum
+      
+      # returns a string reprensentation of the number padded with pad_num to a specified length
+      def pad(length = 3, pad_num = 0)
+        begin
+          self.to_s.rjust(length,pad_num.to_s)
+        rescue
+          self.to_s
+        end
+      end
+
+      # return the value in values that is nearest to the number
+      def to_nearest_value(values = [])
+        return self if values.length == 0
+        value = values.first.to_i rescue self
+        diff = (self-value).abs
+        values.each do |val|
+          if (self-val.to_i).abs < diff
+            diff = (self-val.to_i).abs
+            value = val.to_i
+          end
+        end
+        value
+      end
+
+    end
+  end
+end
+
+class Fixnum
+  include Useful::RubyExtensions::Fixnum
+end
