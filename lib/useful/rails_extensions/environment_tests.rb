98c08fe9df3fd601c3d534100f2ee588412d2d78
Kelly <kelly@kelredd.com>
2009-06-16 14:46:25 -0500

    added some useful rails extensions for doing environment tests

---
+++ lib/useful/rails_extensions/environment_tests.rb
@@ -0,0 +1,57 @@
+module Useful
+  module RailsExtensions
+    module EnvironmentTests
+      
+      module ClassMethods
+        
+        def production? 
+          RAILS_ENV == 'production'
+        end
+        
+        def development? 
+          RAILS_ENV == 'development'
+        end
+        
+      end
+      
+      module InstanceMethods
+        
+        def production? 
+          self.class.production?
+        end
+        
+        def development? 
+          self.class.development?
+        end
+        
+      end
+      
+      def self.included(receiver)
+        receiver.extend         ClassMethods
+        receiver.send :include, InstanceMethods
+      end
+
+    end
+  end
+end
+
+module ActiveRecord
+  class Base
+    include Useful::RailsExtensions::EnvironmentTests
+  end
+end
+module ActionController
+  class Base
+    include Useful::RailsExtensions::EnvironmentTests
+  end
+end
+module ActionView
+  class Base
+    include Useful::RailsExtensions::EnvironmentTests
+  end
+end
+module ActionMailer
+  class Base
+    include Useful::RailsExtensions::EnvironmentTests
+  end
+end
