e7a0676d0c0f1648edf8af97fc7d93bf16fc2eda
Kelly <kelly@kelredd.com>
2009-06-21 00:30:26 -0500

    stupid bug fix

--- lib/useful/ruby_extensions_from_rails/string.rb
+++ lib/useful/ruby_extensions_from_rails/string.rb
@@ -23,7 +23,7 @@ module Useful
           if first_letter_in_uppercase
             lower_case_and_underscored_word.to_s.gsub(/\/(.?)/) { "::#{$1.upcase}" }.gsub(/(?:^|_)(.)/) { $1.upcase }
           else
-            lower_case_and_underscored_word.[0..0].downcase + camelize(lower_case_and_underscored_word)[1..-1]
+            lower_case_and_underscored_word[0..0].downcase + camelize(lower_case_and_underscored_word)[1..-1]
           end
         end
 
