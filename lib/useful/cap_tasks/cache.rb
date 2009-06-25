8a61924f3a34fe1cf496a9f338f34f2d9e61580e
Kelly <kelly@kelredd.com>
2009-06-25 15:16:40 -0500

    cleaned up cap tasks some; bump version for gem build

--- lib/useful/cap_tasks/cache.rb
+++ lib/useful/cap_tasks/cache.rb
@@ -1,11 +1,5 @@
 Capistrano::Configuration.instance.load do
   namespace :cache do
   
-    desc "Update code, run 'rake gems:install'"
-    task :install, :roles => :app do
-      deploy.update_code
-      run "cd #{current_release}; sudo #{fetch(:rake, "rake")} #{"RAILS_ENV=#{rails_env}" if defined?(rails_env)} gems:install"
-    end
-  
   end  
 end
