eb7afb11b312daea558e56847fa4c8d12d6aab47
Kelly <kelly@kelredd.com>
2009-10-06 11:17:25 -0500

    better hash to_http_query_str; some more hash helpers

--- lib/useful/ruby_extensions/hash.rb
+++ lib/useful/ruby_extensions/hash.rb
@@ -45,13 +45,16 @@ module Useful
         self.class.except(self, keys)
       end
       
-      # Determines if a value exists for the provided key(s).  Allows searching in nested hashes
-      def check_value?(*keys)
+      # Returns the value for the provided key(s).  Allows searching in nested hashes
+      def get_value(*keys)
         val = self[keys.first] || self[keys.first.to_s]
         val = self[keys.first.to_s.intern] unless val || keys.first.to_s.empty? || keys.first.kind_of?(Symbol) 
-        return val.check_value?(*keys[1..-1]) if val.kind_of?(Hash) && keys.length > 1
-        return true if val && !val.empty?
-        false
+        val.kind_of?(Hash) && keys.length > 1 ? val.get_value?(keys[1..-1]) : val
+      end
+      # Determines if a value exists for the provided key(s).  Allows searching in nested hashes
+      def check_value?(*keys)
+        val = self.get_value(keys)
+        val && !val.empty? ? true : false
       end
 
       # takes any empty values and makes them nil inline
@@ -65,13 +68,36 @@ module Useful
       #  # => "?id=thomas_hardy"
       #  {:id => 23423, :since => Time.now}.to_http_query_str
       #  # => "?since=Thu,%2021%20Jun%202007%2012:10:05%20-0500&id=23423"
-      def to_http_query_str(opts = {})
-        require 'cgi' unless defined?(CGI) && defined?(CGI::escape)
-        opts[:prepend] ||= '?'
-        opts[:append] ||= ''
-        self.empty? ? '' : "#{opts[:prepend]}#{self.collect{|key, val| "#{key.to_s}=#{CGI.escape(val.to_s)}"}.join('&')}#{opts[:append]}"
+      #  {:id => [1,2]}.to_http_query_str
+      #  # => "?id[]=1&id[]=2"
+      #  {:poo => {:foo => 1, :bar => 2}}.to_http_query_str
+      #  # => "?poo[bar]=2&poo[foo]=1"
+      #  {:poo => {:foo => 1, :bar => {:bar1 => 1, :bar2 => "nasty"}}}.to_http_query_str
+      #  "?poo[bar][bar1]=1&poo[bar][bar2]=nasty&poo[foo]=1"
+      unless {}.respond_to?(:to_http_query_str) 
+        def to_http_query_str(opts = {})
+          require 'cgi' unless defined?(::CGI) && defined?(::CGI::escape)
+          opts[:prepend] ||= '?'
+          opts[:append] ||= ''
+          opts[:key_ns] ||= nil
+          opt_strings = self.collect do |key, val|
+            key_s = opts[:key_ns] ? "#{opts[:key_ns]}[#{key.to_s}]" : key.to_s
+            if val.kind_of?(::Array)
+              val.collect{|i| "#{key_s}[]=#{::CGI.escape(i.to_s)}"}.join('&')
+            elsif val.kind_of?(::Hash)
+              val.to_http_query_str({
+                :prepend => '',
+                :key_ns => key_s,
+                :append => ''
+              })
+            else
+              "#{key_s}=#{::CGI.escape(val.to_s)}"
+            end
+          end 
+          self.empty? ? '' : "#{opts[:prepend]}#{opt_strings.join('&')}#{opts[:append]}"
+        end
       end
-
+      
       def to_html_attrs
         self.empty? ? '' : self.collect{|key, val| "#{key}=\"#{val}\""}.join(' ')
       end
