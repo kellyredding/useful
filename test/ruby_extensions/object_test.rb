706dc0577a627395a79ce4479fb62c819865b032
Kelly <kelly@kelredd.com>
2009-10-14 16:10:54 -0500

    added extended Object tests

---
+++ test/ruby_extensions/object_test.rb
@@ -0,0 +1,27 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'useful/ruby_extensions'
+
+class ObjectTest < Test::Unit::TestCase
+  
+  context "an extended Object" do
+    setup do
+      @obj = Object.new
+      @true = true
+      @false = false
+    end
+    subject { @obj }
+    
+    should_have_instance_methods 'false?', 'true?'
+    
+    should "know if its true" do
+      assert @true.true?
+      assert !@false.true?
+    end
+    should "know if its false" do
+      assert !@true.false?
+      assert @false.false?
+    end
+
+  end
+  
+end
