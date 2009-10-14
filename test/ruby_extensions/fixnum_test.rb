8595ad59d6e6184dd18a31f0f1413b2aab7ee0a8
Kelly <kelly@kelredd.com>
2009-10-14 17:35:53 -0500

    some fixnum extension tests

---
+++ test/ruby_extensions/fixnum_test.rb
@@ -0,0 +1,24 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'useful/ruby_extensions'
+
+class FixnumTest < Test::Unit::TestCase
+  
+  context "an extended Fixnum" do
+    setup do
+      @fixnum = 5
+    end
+    subject { @fixnum }
+    
+    should_have_instance_methods 'pad', 'to_nearest_value'
+    
+    should "pad to 3 wide with zeros by default" do
+      assert_equal "005", subject.pad
+    end
+
+    should "pad to custom width with a custom char" do
+      assert_equal "***5", subject.pad(4, '*')
+      assert_equal "aba5", subject.pad(4, 'ab')
+    end
+  end
+  
+end
