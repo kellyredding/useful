919f2f0ab6c8c1d7f191d23165624e81d1b87986
Kelly <kelly@kelredd.com>
2009-10-14 23:07:14 -0500

    tests and tweaks to bugs the tests revealed, for extended Hash object

--- lib/useful/ruby_extensions/hash.rb
+++ lib/useful/ruby_extensions/hash.rb
@@ -4,107 +4,133 @@ module Useful::RubyExtensions; end
 require 'json' unless defined?(JSON) && defined?(JSON::parse)
 
 module Useful::RubyExtensions::Hash
-
-  module ClassMethods; end
-  def self.included(klass)
-    klass.extend(ClassMethods) if klass.kind_of?(Class)
-  end
-
+  
   module ClassMethods
-
-    def only(hash, *keys)
-      hash.delete_if{ |k,v| !keys.flatten.include?(k) }
-      hash
-    end
-    
-    def except(hash, *keys)
-      hash.delete_if{ |k,v| keys.flatten.include?(k) }
-      hash
-    end
     
     def from_json(string)
       JSON.parse(string)
     end
     
-  end
-
-  # Return a new hash with only keys in *keys
-  def only(*keys)
-    self.class.only(self.clone, keys)
-  end
-  # Destructively remove all keys not in *keys
-  def only!(*keys)
-    self.class.only(self, keys)
-  end
-
-  # Return a new hash with only keys not in *keys
-  def except(*keys)
-    self.class.except(self.clone, keys)
-  end
-  # Destructively remove all keys in *keys
-  def except!(*keys)
-    self.class.except(self, keys)
-  end
-  
-  # Returns the value for the provided key(s).  Allows searching in nested hashes
-  def get_value(*keys)
-    val = self[keys.first] || self[keys.first.to_s]
-    val = self[keys.first.to_s.intern] unless val || keys.first.to_s.empty? || keys.first.kind_of?(Symbol) 
-    val.kind_of?(Hash) && keys.length > 1 ? val.get_value?(keys[1..-1]) : val
-  end
-  # Determines if a value exists for the provided key(s).  Allows searching in nested hashes
-  def check_value?(*keys)
-    val = self.get_value(keys)
-    val && !val.empty? ? true : false
-  end
-
-  # takes any empty values and makes them nil inline
-  def nillify!
-    self.each { |key,value| self[key] = nil if !value.nil? && value.to_s.empty? }
-  end
-
-  # Returns string formatted for HTTP URL encoded name-value pairs.
-  # For example,
-  #  {:id => 'thomas_hardy'}.to_http_query_str 
-  #  # => "?id=thomas_hardy"
-  #  {:id => 23423, :since => Time.now}.to_http_query_str
-  #  # => "?since=Thu,%2021%20Jun%202007%2012:10:05%20-0500&id=23423"
-  #  {:id => [1,2]}.to_http_query_str
-  #  # => "?id[]=1&id[]=2"
-  #  {:poo => {:foo => 1, :bar => 2}}.to_http_query_str
-  #  # => "?poo[bar]=2&poo[foo]=1"
-  #  {:poo => {:foo => 1, :bar => {:bar1 => 1, :bar2 => "nasty"}}}.to_http_query_str
-  #  "?poo[bar][bar1]=1&poo[bar][bar2]=nasty&poo[foo]=1"
-  unless {}.respond_to?(:to_http_query_str) 
-    def to_http_query_str(opts = {})
-      require 'cgi' unless defined?(::CGI) && defined?(::CGI::escape)
-      opts[:prepend] ||= '?'
-      opts[:append] ||= ''
-      opts[:key_ns] ||= nil
-      opt_strings = self.collect do |key, val|
-        key_s = opts[:key_ns] ? "#{opts[:key_ns]}[#{key.to_s}]" : key.to_s
-        if val.kind_of?(::Array)
-          val.collect{|i| "#{key_s}[]=#{::CGI.escape(i.to_s)}"}.join('&')
-        elsif val.kind_of?(::Hash)
-          val.to_http_query_str({
-            :prepend => '',
-            :key_ns => key_s,
-            :append => ''
-          })
-        else
-          "#{key_s}=#{::CGI.escape(val.to_s)}"
-        end
-      end 
-      self.empty? ? '' : "#{opts[:prepend]}#{opt_strings.join('&')}#{opts[:append]}"
+    # Return the hash with only keys in *keys
+    def only(hash, *keys)
+      hash.delete_if{ |k,v| !keys.flatten.include?(k) }
+      hash
     end
+    
+    # Return the hash with only keys NOT in *keys
+    def except(hash, *keys)
+      hash.delete_if{ |k,v| keys.flatten.include?(k) }
+      hash
+    end
+    
+    # takes any empty values and makes them nil
+    def nillify(hash)
+      hash.each do |key,value|
+        if !value.nil? && ( (value.respond_to?('empty?') && value.empty?) || (value.respond_to?('to_s') && value.to_s.empty?) )
+          hash[key] = nil
+        end
+      end
+      hash
+    end
+    
   end
   
-  def to_html_attrs
-    self.empty? ? '' : self.collect{|key, val| "#{key}=\"#{val}\""}.join(' ')
-  end
+  module InstanceMethods
+    
+    def only(*keys)
+      self.class.only(self.clone, keys)
+    end
+    def only!(*keys)
+      self.class.only(self, keys)
+    end
 
+    def except(*keys)
+      self.class.except(self.clone, keys)
+    end
+    def except!(*keys)
+      self.class.except(self, keys)
+    end
+
+    def nillify
+      self.class.nillify(self.clone)
+    end
+    def nillify!
+      self.class.nillify(self)
+    end
+
+    # Returns the value for the provided key(s).  Allows searching in nested hashes
+    def get_value(keys=[])
+      if !keys.respond_to?('empty?') || keys.empty?
+        nil
+      else
+        val = self[keys.first]
+        val.respond_to?('get_value') && keys.length > 1 ? val.get_value(keys[1..-1]) : val
+      end
+    end
+    alias search get_value
+
+    # Determines if a value exists for the provided key(s).  Allows searching in nested hashes
+    def check_value?(keys=[])
+      val = self.get_value(keys)
+      val && !val.empty? ? true : false
+    end
+
+    # Returns string formatted for HTTP URL encoded name-value pairs.
+    # For example,
+    #  {:id => 'thomas_hardy'}.to_http_query_str 
+    #  # => "?id=thomas_hardy"
+    #  {:id => 23423, :since => Time.now}.to_http_query_str
+    #  # => "?since=Thu,%2021%20Jun%202007%2012:10:05%20-0500&id=23423"
+    #  {:id => [1,2]}.to_http_query_str
+    #  # => "?id[]=1&id[]=2"
+    #  {:poo => {:foo => 1, :bar => 2}}.to_http_query_str
+    #  # => "?poo[bar]=2&poo[foo]=1"
+    #  {:poo => {:foo => 1, :bar => {:bar1 => 1, :bar2 => "nasty"}}}.to_http_query_str
+    #  "?poo[bar][bar1]=1&poo[bar][bar2]=nasty&poo[foo]=1"
+    unless {}.respond_to?(:to_http_query_str) 
+      def to_http_query_str(opts = {})
+        require 'cgi' unless defined?(::CGI) && defined?(::CGI::escape)
+        opts[:prepend] ||= '?'
+        opts[:append] ||= ''
+        opts[:key_ns] ||= nil
+        opt_strings = self.collect do |key, val|
+          key_s = opts[:key_ns] ? "#{opts[:key_ns]}[#{key.to_s}]" : key.to_s
+          if val.kind_of?(::Array)
+            val.collect{|i| "#{key_s}[]=#{::CGI.escape(i.to_s)}"}.join('&')
+          elsif val.respond_to?('to_http_query_str')
+            val.to_http_query_str({
+              :prepend => '',
+              :key_ns => key_s,
+              :append => ''
+            })
+          else
+            "#{key_s}=#{::CGI.escape(val.to_s)}"
+          end
+        end 
+        self.empty? ? '' : "#{opts[:prepend]}#{opt_strings.join('&')}#{opts[:append]}"
+      end
+    end
+
+    def to_html_attrs
+      self.empty? ? '' : self.collect{|key, val| "#{key}=\"#{val}\""}.join(' ')
+    end
+
+  end
+  
+  def self.included(receiver)
+    receiver.extend         ClassMethods
+    receiver.send :include, InstanceMethods
+  end
+  
 end
 
 class Hash
   include Useful::RubyExtensions::Hash
+end
+
+# TODO: verify this in a ruby console
+if defined?(::HashWithIndifferentAccess)
+  class HashWithIndifferentAccess
+    include Useful::RubyExtensions::Hash
+  end
 end
---
+++ test/ruby_extensions/hash_test.rb
@@ -0,0 +1,115 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'useful/ruby_extensions'
+
+class HashTest < Test::Unit::TestCase
+  
+  context "an extended Hash" do
+    setup do
+      @hash = { :one => 1, :two => 2, :three => 3, :a => 'a', :b => 'b', :c => 'c' }
+      @nums = { :one => 1, :two => 2, :three => 3 }
+      @nums_string_keys = { 'one' => 1, 'two' => 2, 'three' => 3 }
+      @nums_keys = [:one, :two, :three]
+      @abcs = { :a => 'a', :b => 'b', :c => 'c' }
+      @abcs_keys = [:a, :b, :c]
+      @nilsish = { :nil => nil, :string => '', :not => true, :array => [], :hash => {} }
+      @nillified = { :nil => nil, :string => nil, :not => true, :array => nil, :hash => nil }
+    end
+    subject { @hash }
+    
+    
+    should_have_class_methods 'from_json'
+    
+    should "be able to be instantiated from a JSON string" do
+      assert_equal @nums_string_keys, Hash.from_json("{ \"one\": 1, \"two\": 2, \"three\": 3 }")
+    end
+    
+    
+    should_have_class_methods 'only', 'except', 'nillify'
+    should_have_instance_methods 'only', 'only!', 'except', 'except!', 'nillify', 'nillify!'
+
+    should "should only return certain keys at the class level" do
+      assert_equal @nums, Hash.only(subject, @nums_keys)
+    end
+    should "should only return certain keys at the instance level" do
+      assert_equal @abcs, subject.only(@abcs_keys)
+    end
+    should "should destructively only return certain keys at the instance level" do
+      subject.only!(@abcs_keys)
+      assert_equal @abcs, subject
+    end
+
+    should "should return all keys except certain ones at the class level" do
+      assert_equal @abcs, Hash.except(subject, @nums_keys)
+    end
+    should "should return all keys except certain ones at the instance level" do
+      assert_equal @nums, subject.except(@abcs_keys)
+    end
+    should "should destructively return all keys except certain ones at the instance level" do
+      subject.except!(@abcs_keys)
+      assert_equal @nums, subject
+    end
+
+    should "should nillify at the class level" do
+      assert_equal @nillified, Hash.nillify(@nilsish)
+    end
+    should "should nillify at the instance level" do
+      assert_equal @nillified, @nilsish.nillify
+    end
+    should "should destructively nillify at the instance level" do
+      @nilsish.nillify!
+      assert_equal @nillified, @nilsish
+    end
+
+
+    should_have_instance_methods 'get_value', 'search', 'check_value?'
+    
+    should "be able to get nested values" do
+      @nested = { 1 => { 2 => { 3 => 4 } } }
+      assert_equal nil, @nested.get_value 
+      assert_equal nil, @nested.get_value([])
+      @expected = { 2 => { 3 => 4 } }
+      assert_equal @expected, @nested.get_value([1]) 
+      @expected = { 3 => 4 }
+      assert_equal @expected, @nested.get_value([1,2]) 
+      assert_equal 4, @nested.get_value([1,2,3]) 
+    end
+    
+    should "be able to check nested values" do
+      @nested = { 1 => { 2 => { 3 => [], 4 => nil } } }
+      assert !@nested.check_value?
+      assert !@nested.check_value?([]) 
+      assert @nested.check_value?([1]) 
+      assert @nested.check_value?([1,2]) 
+      assert !@nested.check_value?([1,2,3]) 
+      assert !@nested.check_value?([1,2,4]) 
+    end
+    
+    
+    should_have_instance_methods 'to_http_query_str', 'to_html_attrs'
+
+    should "be able to convert to proper http query string" do
+      @expected = "?name=thomas+hardy+%2F+thomas+handy"
+      assert_equal @expected, {:name => 'thomas hardy / thomas handy'}.to_http_query_str 
+      @expected = "?since=2009-10-14&id=23423"
+      assert_equal @expected, {:id => 23423, :since => "2009-10-14"}.to_http_query_str
+      @expected = "?id[]=1&id[]=2"
+      assert_equal @expected, {:id => [1,2]}.to_http_query_str
+      @expected = "?poo[foo]=1&poo[bar]=2"
+      assert_equal @expected, {:poo => {:foo => 1, :bar => 2}}.to_http_query_str
+      @expected = "?poo[foo]=1&poo[bar][bar1]=1&poo[bar][bar2]=nasty"
+      assert_equal @expected, {:poo => {:foo => 1, :bar => {:bar1 => 1, :bar2 => "nasty"}}}.to_http_query_str
+      @expected = "name=johnson&this=suffix"
+      assert_equal @expected, {:name => 'johnson'}.to_http_query_str(:prepend => '', :append => '&this=suffix')
+    end
+
+    should "be able to convert to proper html attribute string" do
+      @expected = ""
+      assert_equal @expected, {}.to_html_attrs 
+      @expected = "class=\"test\" id=\"test_1\""
+      assert_equal @expected, {:class => "test", :id => "test_1"}.to_html_attrs
+    end
+
+
+  end
+  
+end
