89e517ab164765828c61848026e7e9fa71faebd5
Kelly <kelly@kelredd.com>
2009-11-02 10:51:09 -0600

    added some additional base shoulda macros

--- lib/useful/shoulda_macros/test_unit.rb
+++ lib/useful/shoulda_macros/test_unit.rb
@@ -29,6 +29,30 @@ module Useful::ShouldaMacros::TestUnit
     end
   end unless Test::Unit::TestCase.method_defined? :should_have_class_methods
   
+  def should_have_readers(*readers)
+    get_options!(readers)
+    klass = described_type
+    readers.each do |reader|
+      should_have_instance_method reader
+    end
+  end unless Test::Unit::TestCase.method_defined? :should_have_readers
+  
+  def should_have_writers(*writers)
+    get_options!(writers)
+    klass = described_type
+    writers.each do |writer|
+      should_have_instance_method "#{writer}="
+    end
+  end unless Test::Unit::TestCase.method_defined? :should_have_writers
+  
+  def should_have_accessors(*accessors)
+    get_options!(accessors)
+    klass = described_type
+    accessors.each do |accessor|
+      should_have_instance_method accessor, "#{accessor}="
+    end
+  end unless Test::Unit::TestCase.method_defined? :should_have_accessors
+  
 end
 
 Test::Unit::TestCase.extend(Useful::ShouldaMacros::TestUnit) if defined? Test::Unit::TestCase
